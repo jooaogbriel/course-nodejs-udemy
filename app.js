e8b1f099de351ff16726e4d907238dc6505bce8a
João Gonçalves <joao_i0807@outlook.com>
2024-03-14 15:53:25 -0300

    feat: iniciando express por debaixo dos panos

--- app.js
+++ app.js
@@ -1,6 +1,17 @@
 const http = require('http');
-
+const express = require('express')
 const routes = require('./routes')
+const bodyParser = require('body-parser');
+
+const app = express()
+app.use(bodyParser.json());
+app.use('/add-product', (req, res, next) => {
+    res.send('<form action="/product"><input type="text" name="title"><button type="submit">Send</button></form>')
+})
+app.use('/product', (req, res, next)=> {
+    console.log(req.body)
+    res.redirect('/')
+})
 
 const server = http.createServer(routes.handler)
-server.listen(3003)
+app.listen(3003)
